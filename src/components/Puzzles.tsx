a8d6014b56892a98d5d35decad43df994911d547
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-12-16 17:21:42 +0100

    Puzzles component

--- src/components/Puzzles.tsx
+++ src/components/Puzzles.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, List, ListItem, Text } from "@chakra-ui/react";
+import { Box, Center, Heading, List, ListItem, Text } from "@chakra-ui/react";
 
 const Puzzles: React.FC = () => {
   return (
@@ -16,7 +16,14 @@ const Puzzles: React.FC = () => {
       </Heading>
       <List spacing={3}>
         <ListItem>1. Cyfra ukryta pod Tobą.</ListItem>
-        <ListItem>2. Rozwiąż całkę:</ListItem>
+        <ListItem>
+          2. Rozwiąż całkę: <br />
+          <Center>
+            &#8747;&#8747;&#8747;(xy) dV <br />
+            V=&#123;(x,y,z)&#x2223; z &#x2264; 36 &#x2212; 9x &#x2212; 4y, z
+            &#x2265; 0, x&#x2265;0, y&#x2265;0&#125;
+          </Center>
+        </ListItem>
         <ListItem>3. Sprawdź pod łóżkiem (w Bełchatowie).</ListItem>
         <ListItem>4. Wypadałoby się pouczyć angielskiego.</ListItem>
       </List>
